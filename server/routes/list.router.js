f923d911314abf19ca3345205bbb568f9b29c8f7
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-24 14:47:56 -0500

    added our GETS

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -6,3 +6,17 @@ const pool = require('../modules/pool.js');
 
 
 module.exports = router;
+
+router.get('/', (req, res) => {
+    
+    const sqlText = `SELECT * FROM cart ORDER BY name, purchased DESC;`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, {useEffect, useState}from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+let [itemList, setItemList] = useState([]);
+
+const getItem = ()=> {
+axios.get('/list')
+.then((response)=> {
+console.log('the whole response', response);
+console.log('the data', response.data);
+
+setItemList(response.data);
+}).catch(function (err){
+
+    console.log('ERROR in GET', err);
+});
+}
+
+
     return (
         <div className="App">
             <Header />

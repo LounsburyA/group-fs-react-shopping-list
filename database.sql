e42fc9a3c76245d4a1109c73a925965aa22d13e7
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-24 14:29:49 -0500

    added our database name to pool.js

--- database.sql
+++ database.sql
@@ -1,2 +1,18 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+
+
+CREATE TABLE cart(
+id SERIAL PRIMARY KEY,
+item VARCHAR(80),
+quantity FLOAT,
+unit VARCHAR(20),
+purchased BOOLEAN);
+
+
+INSERT INTO cart (item, quantity, unit)
+VALUES ('oatmilk', 2, 'gallon'),
+('tacos', 11, 'kilos');
+
+

7ffc346840bcf334d8732a655d4df672dccbdd21
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-03-25 11:12:39 -0500

    add POST

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -8,7 +8,7 @@ const pool = require('../modules/pool.js');
 module.exports = router;
 
 router.get('/', (req, res) => {
-    
+
     const sqlText = `SELECT * FROM cart ORDER BY name, purchased DESC;`;
     pool.query(sqlText)
         .then((result) => {
@@ -19,4 +19,21 @@ router.get('/', (req, res) => {
             console.log(`Error making database query ${sqlText}`, error);
             res.sendStatus(500); // Good server always responds
         })
+})
+
+router.post('/', (req, res) => {
+    const newItem = req.body.item;
+    const sqlText = `INSERT INTO cart (item, quantity, unit) VALUES ($1,$2,3)`;
+    const values = [item.itemName, item.quantity, item.unit]
+    pool.query(sqlText, values)
+        .then((result) => {
+            res.sendStatus(201);
+            console.log(req.body);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        });
+
+
 })
